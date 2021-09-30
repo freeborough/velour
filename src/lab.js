ddb3d51fdf7c528a8aa56ebbc700affae6bedf4c
freeborough <andy@freeborough.com>
2021-09-30 19:03:51 +0100

    Using delta time.

--- src/lab.js
+++ src/lab.js
@@ -1,15 +1,18 @@
 import DeltaLoop from "./DeltaLoop.js";
 
-var count = 0;
-function doCount() {
+var count = 0n;
+var totalTime = 0n;
+
+function doCount(dt) {
   count++;
+  totalTime += dt;
 }
 
 const deltaLoop = new DeltaLoop(doCount);
 
 setTimeout(() => {
   deltaLoop.stop();
-  console.log(`We completed ${count} iterations.`);
+  console.log(`We completed ${count} iterations in ${totalTime} nano seconds, averaging ${totalTime/count} nano seconds per iteration.`);
   process.exit(0);
 }, 1000);
 
